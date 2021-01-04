801d8b491e186da75f1418d71733470407e926bc
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-05 00:25:16 +0100

    babel setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(4000, () => {
+  console.log("App is up and running");
+});
