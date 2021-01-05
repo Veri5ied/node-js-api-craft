ffa8556aee0981ae4d0cf001e9b3e984020a6bfb
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-05 10:50:15 +0100

    disabling x-powered-by

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import sampleRouter from "./resources/sample/sample.router";
 const app = express();
 const PORT = process.env.PORT || 4200;
 
+app.disable("x-powered-by");
+
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
