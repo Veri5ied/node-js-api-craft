7c9fe70cdb41e329051a4096b05ba72f62e75955
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-05 14:45:18 +0100

    database connection

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
+import options from "../config/dev";
 
-export const connect = async () => {
-  await mongoose.connect();
+export const connect = (url = options.dbUrl, opts = {}) => {
+  return mongoose.connect(url, { ...opts, useNewUrlParser: true });
 };
