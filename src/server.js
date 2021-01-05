c38bc2213418dbc0265a7e9ca58b274125b281ee
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-05 10:35:10 +0100

    mounting router on server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
+import sampleRouter from "./resources/sample/sample.router";
 
 const app = express();
 const PORT = process.env.PORT || 4200;
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use("/api/v1/sample", sampleRouter);
 
 export const start = async () => {
   app.listen(PORT, () => {
