3d0d84bd014c75960a4a970c8c1c0544d171e94e
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-12 19:59:39 +0100

    hashing passwords

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -1,6 +1,55 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const UserSchema = new mongoose.Schema({}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.checkPassword = function (password) {
+  const passwordHashed = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHashed, (err, same) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(same);
+    });
+  });
+};
 
 export const User = mongoose.model("user", userSchema);
